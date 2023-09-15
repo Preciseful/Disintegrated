0a4792d2c402070d0e0445b95794c2870c2a57ae
Developful <preciseful1@gmail.com>
2023-09-15 23:37:13 +0300

    Ping command.

---
+++ Disintegrated/Commands/Ping.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using Discord.Interactions;
+
+namespace Bot.Commands; 
+
+public class Ping : InteractionModuleBase<SocketInteractionContext>{
+    private CommandHandler handler;
+
+    public Ping(CommandHandler handler) => this.handler = handler;
+
+    [SlashCommand("ping", "Get discord bot's latency.")]
+    public async Task Invoke() {
+        await DeferAsync(true);
+        Embed embed = new EmbedBuilder()
+            .WithTitle("Pong! :ping_pong:")
+            .WithDescription("Latency: " + Context.Client.Latency.ToString() + "ms.")
+            .WithCurrentTimestamp()
+            .WithColor(Color.Blue)
+            .Build();
+
+        await ModifyOriginalResponseAsync(m => m.Embed = embed);
+    }
+}
